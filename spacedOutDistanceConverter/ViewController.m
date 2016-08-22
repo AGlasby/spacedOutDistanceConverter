e925f8d3f6886c3d24fd4b7346a8245c5505b4b0
Alan Glasby <alan@glasby.me.uk>
2016-08-22 17:56:09 +0100

    UI implemented

--- spacedOutDistanceConverter/ViewController.m
+++ spacedOutDistanceConverter/ViewController.m
@@ -10,9 +10,29 @@
 
 @interface ViewController ()
 
+@property (weak, nonatomic) IBOutlet UILabel *unitsLbl;
+
+@property (weak, nonatomic) IBOutlet UISegmentedControl *unitSelectorSegCntrl;
+
+@property (weak, nonatomic) IBOutlet UILabel *outputLbl;
+
 @end
 
 @implementation ViewController
+
+
+- (IBAction)distanceInputFld:(UITextField *)sender {
+    
+}
+
+- (IBAction)convertBtn:(id)sender {
+    
+    NSMutableString *buf = [NSMutableString new];
+    [buf appendString: @"Clicked"];
+    self.outputLbl.text = buf;
+    
+}
+
 
 - (void)viewDidLoad {
     [super viewDidLoad];
