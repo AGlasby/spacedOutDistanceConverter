4bd817c27fd3486857480834fb2388f55c455fbc
Alan Glasby <alan@glasby.me.uk>
2016-08-22 17:38:32 +0100

    Initial Commit

---
+++ spacedOutDistanceConverter/ViewController.m
@@ -0,0 +1,27 @@
+//
+//  ViewController.m
+//  spacedOutDistanceConverter
+//
+//  Created by Alan on 22/08/2016.
+//  Copyright © 2016 Alan Glasby. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
